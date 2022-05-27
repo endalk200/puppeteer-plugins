39bae1965a5c4362efd65ad193692b060703923d
endalk200 <eb808826@gmail.com>
2022-05-27 10:02:57 +0300

    chore: added CustomErro class and added new fields

--- packages/puppeteer-plugins/src/lib/errors.ts
+++ packages/puppeteer-plugins/src/lib/errors.ts
@@ -1,10 +1,23 @@
 import { runIfFn } from "./utils";
 
 /**
+ * @public
+ */
+export class CustomError extends Error {
+    errorCode: string;
+
+    constructor(message?: string) {
+        super(message);
+
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
+
+/**
  * InputValidationError is an error thrown when the type operation fails to
  * set the input value to the expected value.
  */
-export class InputValidationError extends Error {
+export class InputValidationError extends CustomError {
     constructor(
         expectedValue: string | (() => string),
         actualValue: string | (() => string)
@@ -16,6 +29,7 @@ export class InputValidationError extends Error {
         );
 
         this.name = "InputValidationError";
+        this.errorCode = "INPUT_VALIDATION_ERROR";
     }
 
     getSource() {
@@ -27,11 +41,12 @@ export class InputValidationError extends Error {
     }
 }
 
-export class ElementNotFoundError extends Error {
+export class ElementNotFoundError extends CustomError {
     constructor(selector: string) {
         super(`Element with selector "${selector}" not found for given timeout period`);
 
         this.name = "ElementNotFoundError";
+        this.errorCode = "ELEMENT_NOT_FOUND_ERROR";
     }
 
     getSource() {
@@ -43,9 +58,12 @@ export class ElementNotFoundError extends Error {
     }
 }
 
-export class UnsupportedHTMLTag extends Error {
+export class UnsupportedHTMLTag extends CustomError {
     constructor(tag: string) {
         super(`Unsupported HTML tag: ${tag}`);
+
+        this.name = "UnsupportedHTMLTag";
+        this.errorCode = "UNSUPPORTED_HTML_TAG";
     }
 
     getSource() {
